36485b85a3b933e8b9b14d63c0dcdf8412128612
JosephNaa <js.pekah@gmail.com>
2021-10-18 22:48:15 +0900

    10/18 문제풀이 - 나요셉

---
+++ 10월_3주차/BOJ2003_수들의합2_나요셉.java
@@ -0,0 +1,31 @@
+package com.js.pekah.algorithms.d1018;
+
+import java.util.Scanner;
+
+public class BOJ2003_수들의합2_나요셉 {
+	static int N;
+	static long M;
+	static int arr[];
+	static int result, start, end, sum;
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextLong();
+		arr = new int[N+1];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		while (end <= N) {
+			if (sum >= M) sum -= arr[start++];
+			else if (sum < M) sum += arr[end++];
+			
+			if (sum == M) result++;
+		}
+		
+		System.out.println(result);
+	}
+}
